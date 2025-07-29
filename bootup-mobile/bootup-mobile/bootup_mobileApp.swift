b9d08c9b4cb6afe78917b36872d6288dffa9bf21
Jacob Kelly <jskelly2021@gmail.com>
2025-07-29 00:21:48 -0500

    Created a hello world SwiftUI app

---
+++ bootup-mobile/bootup-mobile/bootup_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  bootup_mobileApp.swift
+//  bootup-mobile
+//
+//  Created by Jacob Kelly on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct bootup_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bootup-mobile/bootup-mobileTests/bootup_mobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  bootup_mobileTests.swift
+//  bootup-mobileTests
+//
+//  Created by Jacob Kelly on 7/29/25.
+//
+
+import Testing
+@testable import bootup_mobile
+
+struct bootup_mobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
